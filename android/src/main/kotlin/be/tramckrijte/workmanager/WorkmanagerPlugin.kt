95128e880c6ee12861ec6217efcc21cedf9f59ea
Tim Rijckaert <timpsiih@hotmail.com>
2019-08-01 06:50:05 +0200

    Working proof of concept

--- android/src/main/kotlin/be/tramckrijte/workmanager/WorkmanagerPlugin.kt
+++ android/src/main/kotlin/be/tramckrijte/workmanager/WorkmanagerPlugin.kt
@@ -2,18 +2,24 @@ package be.tramckrijte.workmanager
 
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class WorkmanagerPlugin(private val workmanagerCallHandler: WorkmanagerCallHandler) : MethodCallHandler {
 
     companion object {
+        lateinit var pluginRegistryCallback: PluginRegistry.PluginRegistrantCallback
+
         @JvmStatic
         fun registerWith(registrar: Registrar) {
             val channel = MethodChannel(registrar.messenger(), "be.tramckrijte.workmanager/foreground_channel_work_manager")
             channel.setMethodCallHandler(WorkmanagerPlugin(WorkmanagerCallHandler(registrar.activeContext())))
+        }
+
+        fun setPluginRegistrantCallback(pluginRegistryCallback: PluginRegistry.PluginRegistrantCallback) {
+            WorkmanagerPlugin.pluginRegistryCallback = pluginRegistryCallback
         }
     }
 
