f99741caf26b93d7b2b3bbe6aae95d4496af67b3
Kymer <kymer.dev@icloud.com>
2019-08-13 16:44:04 +0200

    Show emoji thumbnail in debug notification 🚀

--- ios/Classes/DebugNotificationHelper.swift
+++ ios/Classes/DebugNotificationHelper.swift
@@ -48,6 +48,9 @@ struct DebugNotificationHelper {
         notification.title = title
         notification.body = body
         notification.threadIdentifier = SwiftWorkmanagerPlugin.identifier
+        if let thumbnail = createThumbnail(emoji: "🚀") {
+            notification.attachments = [thumbnail]
+        }
         let immediateFutureTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
         let notificationRequest = UNNotificationRequest(identifier: identifier, content: notification, trigger: immediateFutureTrigger)
         
@@ -74,6 +77,71 @@ struct DebugNotificationHelper {
     
     private static var logPrefix: String {
         return "\(String(describing: SwiftWorkmanagerPlugin.self)) - \(DebugNotificationHelper.self)"
+    }
+    
+    @available(iOS 10.0, *)
+    private static func createThumbnail(emoji: String) -> UNNotificationAttachment? {
+        let name = "thumbnail"
+        let thumbnailFrame = CGRect(x: 0, y: 0, width: 150, height: 150)
+        let thumbnail = UIView(frame: thumbnailFrame)
+        thumbnail.isOpaque = false
+        let label = UILabel(frame: thumbnailFrame)
+        label.text = emoji
+        label.font = UIFont.systemFont(ofSize: 125)
+        label.textAlignment = .center
+        thumbnail.addSubview(label)
+        
+        do {
+            let thumbnailImage = try thumbnail.renderAsImage()
+            let localURL = try thumbnailImage.persist(fileName: name)
+            return try UNNotificationAttachment(identifier: "\(SwiftWorkmanagerPlugin.identifier).\(name)", url: localURL, options: nil)
+        } catch {
+            logInfo("\(logPrefix) \(#function) something went wrong creating a thumbnail for local debug notification")
+            return nil
+        }
+        
+    }
+    
+}
+
+private extension UIView {
+    
+    func renderAsImage() throws -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.isOpaque, 0.0)
+        defer { UIGraphicsEndImageContext() }
+        
+        guard let context = UIGraphicsGetCurrentContext() else {
+            throw error.noCurrentGraphicsContextFound
+        }
+        self.layer.render(in: context)
+        guard let image = UIGraphicsGetImageFromCurrentImageContext() else {
+            throw error.noCurrentGraphicsContextFound
+        }
+        
+        return image
+    }
+    
+    enum error: Error {
+        case noCurrentGraphicsContextFound
+    }
+}
+
+private extension UIImage {
+    
+    func persist(fileName: String, in directory: URL = URL(fileURLWithPath: NSTemporaryDirectory())) throws -> URL {
+        let directoryURL = directory.appendingPathComponent(SwiftWorkmanagerPlugin.identifier, isDirectory: true)
+        let fileURL = directoryURL.appendingPathComponent("\(fileName).png")
+        try FileManager.default.createDirectory(at: directoryURL, withIntermediateDirectories: true, attributes: nil)
+        guard let imageData = UIImagePNGRepresentation(self) else {
+            throw error.cannotRepresentAsPNG(self)
+        }
+        try imageData.write(to: fileURL)
+        
+        return fileURL
+    }
+    
+    enum error: Error {
+        case cannotRepresentAsPNG(UIImage)
     }
     
 }
