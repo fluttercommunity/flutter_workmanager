eadda1314a122fd4e84a4f24c5757fb5b49fcb99
Sebastian Roth <sebastian@nextbase.co.uk>
2020-06-30 09:30:45 +0100

    Reactivate the tests

--- lib/src/workmanager.dart
+++ lib/src/workmanager.dart
@@ -88,17 +88,18 @@ class Workmanager {
   /// This call is required if you wish to use the [WorkManager] plugin.
   /// [callbackDispatcher] is a top level function which will be invoked by Android
   /// [isInDebugMode] true will post debug notifications with information about when a task should have run
-  static Future<void> initialize(final Function callbackDispatcher, {
+  static Future<void> initialize(
+    final Function callbackDispatcher, {
     final bool isInDebugMode = false,
   }) async {
     Workmanager._isInDebugMode = isInDebugMode;
     final callback = PluginUtilities.getCallbackHandle(callbackDispatcher);
     assert(callback != null,
-    "The callbackDispatcher needs to be either a static function or a top level function to be accessible as a Flutter entry point.");
+        "The callbackDispatcher needs to be either a static function or a top level function to be accessible as a Flutter entry point.");
     final int handle = callback.toRawHandle();
     await _foregroundChannel.invokeMethod(
       'initialize',
-      JsonMapperHelper._toInitializeMethodArgument(
+      JsonMapperHelper.toInitializeMethodArgument(
         isInDebugMode: _isInDebugMode,
         callbackHandle: handle,
       ),
@@ -109,19 +110,20 @@ class Workmanager {
   /// A [uniqueName] is required so only one task can be registered.
   /// The [taskName] is the value that will be returned in the [BackgroundTaskHandler]
   /// The [inputData] is the input data for task. Valid value types are: int, bool, double, String and their list
-  static Future<void> registerOneOffTask(final String uniqueName,
-      final String taskName, {
-        final String tag,
-        final ExistingWorkPolicy existingWorkPolicy,
-        final Duration initialDelay = _noDuration,
-        final Constraints constraints,
-        final BackoffPolicy backoffPolicy,
-        final Duration backoffPolicyDelay = _noDuration,
-        final Map<String, dynamic> inputData,
-      }) async =>
+  static Future<void> registerOneOffTask(
+    final String uniqueName,
+    final String taskName, {
+    final String tag,
+    final ExistingWorkPolicy existingWorkPolicy,
+    final Duration initialDelay = _noDuration,
+    final Constraints constraints,
+    final BackoffPolicy backoffPolicy,
+    final Duration backoffPolicyDelay = _noDuration,
+    final Map<String, dynamic> inputData,
+  }) async =>
       await _foregroundChannel.invokeMethod(
         "registerOneOffTask",
-        JsonMapperHelper._toRegisterMethodArgument(
+        JsonMapperHelper.toRegisterMethodArgument(
           isInDebugMode: _isInDebugMode,
           uniqueName: uniqueName,
           taskName: taskName,
@@ -141,20 +143,21 @@ class Workmanager {
   /// a [frequency] is not required and will be defaulted to 15 minutes if not provided.
   /// a [frequency] has a minimum of 15 min. Android will automatically change your frequency to 15 min if you have configured a lower frequency.
   /// The [inputData] is the input data for task. Valid value types are: int, bool, double, String and their list
-  static Future<void> registerPeriodicTask(final String uniqueName,
-      final String taskName, {
-        final Duration frequency,
-        final String tag,
-        final ExistingWorkPolicy existingWorkPolicy,
-        final Duration initialDelay = _noDuration,
-        final Constraints constraints,
-        final BackoffPolicy backoffPolicy,
-        final Duration backoffPolicyDelay = _noDuration,
-        final Map<String, dynamic> inputData,
-      }) async =>
+  static Future<void> registerPeriodicTask(
+    final String uniqueName,
+    final String taskName, {
+    final Duration frequency,
+    final String tag,
+    final ExistingWorkPolicy existingWorkPolicy,
+    final Duration initialDelay = _noDuration,
+    final Constraints constraints,
+    final BackoffPolicy backoffPolicy,
+    final Duration backoffPolicyDelay = _noDuration,
+    final Map<String, dynamic> inputData,
+  }) async =>
       await _foregroundChannel.invokeMethod(
         "registerPeriodicTask",
-        JsonMapperHelper._toRegisterMethodArgument(
+        JsonMapperHelper.toRegisterMethodArgument(
           isInDebugMode: _isInDebugMode,
           uniqueName: uniqueName,
           taskName: taskName,
@@ -190,7 +193,8 @@ class Workmanager {
 
 /// A helper object to convert the selected options to JSON format. Mainly for testability.
 class JsonMapperHelper {
-  static Map<String, Object> _toRegisterMethodArgument({
+  @visibleForTesting
+  static Map<String, Object> toRegisterMethodArgument({
     final bool isInDebugMode,
     final String uniqueName,
     final String taskName,
@@ -244,7 +248,8 @@ class JsonMapperHelper {
     };
   }
 
-  static Map<String, Object> _toInitializeMethodArgument({
+  @visibleForTesting
+  static Map<String, Object> toInitializeMethodArgument({
     final bool isInDebugMode,
     final int callbackHandle,
   }) {
@@ -256,8 +261,5 @@ class JsonMapperHelper {
   }
 
   static String _enumToString(final dynamic enumeration) =>
-      enumeration
-          ?.toString()
-          ?.split('.')
-          ?.last;
+      enumeration?.toString()?.split('.')?.last;
 }
