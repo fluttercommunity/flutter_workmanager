70f944409210fc9c142b489ae87f6a730f579b00
Sebastian Roth <sebastian@nextbase.co.uk>
2021-07-23 10:49:56 +0100

    Allow to fail scheduling in CI simulator

--- example/integration_test/workmanager_integration_test.dart
+++ example/integration_test/workmanager_integration_test.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:workmanager/workmanager.dart';
@@ -11,9 +14,23 @@ void callbackDispatcher() {
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('initialize & schedule task', (WidgetTester tester) async {
+  testWidgets('initialize & schedule task - android',
+      (WidgetTester tester) async {
     final wm = Workmanager();
     await wm.initialize(callbackDispatcher);
     await wm.registerOneOffTask('taskId', 'taskName');
-  });
+  }, skip: !Platform.isAndroid);
+
+  testWidgets('initialize & schedule task - iOS', (WidgetTester tester) async {
+    final wm = Workmanager();
+    await wm.initialize(callbackDispatcher);
+    try {
+      await wm.registerOneOffTask('taskId', 'taskName');
+    } on PlatformException catch (e) {
+      if (e.toString() !=
+          'PlatformException(bgTaskSchedulingFailed(Error Domain=BGTaskSchedulerErrorDomain Code=1 "(null)")') {
+        rethrow;
+      }
+    }
+  }, skip: !Platform.isIOS);
 }
