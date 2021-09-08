03014cc5b4881efa0c77d31e730fe7affcb7f5e0
Sebastian Roth <sebastian@nextbase.co.uk>
2021-09-08 16:01:56 +0100

    Restore the NetworkType parser on iOS, ensure the correct argument is used & add unit test

---
+++ example/ios/RunnerTests/workmanagerTests.swift
@@ -0,0 +1,24 @@
+//
+//  workmanagerTests.swift
+//  workmanagerTests
+//
+//  Created by Sebastian Roth on 08/09/2021.
+//  Copyright © 2021 The Chromium Authors. All rights reserved.
+//
+
+import XCTest
+
+@testable import workmanager
+
+class workmanagerTests: XCTestCase {
+
+    func testNetworkType() throws {
+        XCTAssertEqual(NetworkType.connected, NetworkType(fromDart: "connected"))
+        XCTAssertEqual(NetworkType.metered, NetworkType(fromDart: "metered"))
+        XCTAssertEqual(NetworkType.notRequired, NetworkType(fromDart: "not_required"))
+        XCTAssertEqual(NetworkType.notRoaming, NetworkType(fromDart: "not_roaming"))
+        XCTAssertEqual(NetworkType.temporarilyUnmetered, NetworkType(fromDart: "temporarily_unmetered"))
+        XCTAssertEqual(NetworkType.unmetered, NetworkType(fromDart: "unmetered"))
+    }
+
+}
