a4c02ef252feca4418ba0fdbf45889f772073d4c
Sebastian Roth <sebastian.roth@gmail.com>
2022-09-06 09:27:53 +0100

    Add a few more places for @pragma

--- example/integration_test/workmanager_integration_test.dart
+++ example/integration_test/workmanager_integration_test.dart
@@ -5,6 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:workmanager/workmanager.dart';
 
+@pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
     return true;
