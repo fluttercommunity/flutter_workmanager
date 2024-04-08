b783000c18f6e066f3d81d5ecc352f3ce61860f6
Absaar <absarhshah@gmail.com>
2024-04-08 23:09:12 +0200

    feat!: iOS major enhancements BGAppRefreshTask, BGProcessingTask, beginBackgroundTask, printScheduledTasks (#511)
    * fix:Update Workmanager iOS because no callback in Background on iOS real device, added 30sec BGAppRefresh, Updated example #396
    
    * added permissionhandler an requests for iOS
    added alert and MaterialApp to workmanager when no iOS permissions activated
    
    * fixed errormessage on xcode
    
    * feat:Added check for background refresh permissions #441
    
    * text to display task event dates (show prefs) added.
    
    * fixed workmanager iOS Part
    fixed BGProcessing
    fixed inputdata in callback on task
    clarified timings
    
    * fixed warning dead code and ! check
    
    * improved Task description (hints)
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Format readme iOS examples
    
    * Improve code documentation
    
    * Cleanups in SwiftWorkmanagerPlugin.swift
    * Use logInfo instead of prints and NSLog
    * Log unnecessary logs only in debug mode
    * Remove unnecessary logs
    * Remove isInitalized flag in SwiftWorkmanagerPlugin which was not set to true anywhere
    
    * * iOS, Rename registeriOSBackgroundProcessingTask to a generic name registerProcessingTask to be consistent with rest of the plugin and possible future Android implementation
    * iOS, Rename wrongly named startOnOffTask to startOneOffTask
    
    * * Cleanup code to make it more close to original plugin so that change size is reduced and it will make it easy to review
    * Change new task identifier to be consistent with existing ones e.g. instead of app.workmanager... use be.tramckrijte...
    * Documentation update
    * Remove unnecessary logs, comments etc which were added in PRs which were not merged, and cleanup unnecessary code
    * Revert using a custom log helper OS file to use the plugins existing shared prefs
    * Bump example flutter sdk to < 4 instead of < 3
    
    * Add task identifiers to iOS AppRefresh and ProcessingTask so that user can define task names instead of using hardcoded names
    
    * * iOS AppRefresh task interval should be 15 minutes
    * Documentation update
    
    * Initialize should not auto open App settings if background refresh permission is not assigned.
    Initialize should return result
    
    * Continue work on task identifiers for iOS AppRefresh and ProcessingTask.
    * Temporarily commented old iOS background fetch
    
    * Fix extra commas on iOS
    
    * New iOS feature printScheduledTasks to print details of un-executed scheduled tasks. To be used during development/debugging.
    Format readme to improve readability
    
    * iOS Periodic and processing tasks will be immediately scheduled, instead of waiting for App to go to background. Since doing on backgrounding will keep on changing earliest begin date.
    * Add printScheduledTasks to example app
    * Format example code
    
    * Option to set frequency for iOS periodic tasks in AppDelegate.swift
    * Add initialDelay support for Workmanager.registerProcessingTask
    * Remove unnecessary WorkmanagerPlugin.registerBGProcessingTask calls from AppDelegate.swift
    * Cleanup unused params from Workmanager.registerProcessingTask
    * Update readme and iOS setup as per new iOS developments
    * Create migration steps for iOS Workmanager.registerOneOffTask to Workmanager.registerProcessingTask
    
    * Update iOS docs
    
    * TODO for cleanups later
    
    ---------
    
    Co-authored-by: Lars Huth <lars@huth-familie.de>
    Co-authored-by: xunreal75 <xunreal75@users.noreply.github.com>
    Co-authored-by: Ioseph Magno <83366073+iosephmagno@users.noreply.github.com>
    Co-authored-by: delfme <53510751+delfme@users.noreply.github.com>

--- README.md
+++ README.md
@@ -73,7 +73,7 @@ void callbackDispatcher() {
 ```
 
 Android tasks are identified using their `taskName`.
-iOS tasks are identitied using their `taskIdentifier`.
+iOS tasks are identified using their `taskIdentifier`.
 
 However, there is an exception for iOS background fetch: `Workmanager.iOSBackgroundTask`, a constant for iOS background fetch task.
 
@@ -93,24 +93,118 @@ Refer to the example app for a successful, retrying and a failed task.
 
 # iOS specific setup and note
 
-iOS supports **One off tasks** with a few basic constraints:
+Initialize Workmanager only once.
+Background app refresh can only be tested on a real device, it cannot be tested on a simulator.
+
+### Migrate to 0.6.x
+Version 0.6.x of this plugin has some breaking changes for iOS:
+- Workmanager.registerOneOffTask was previously using iOS **BGProcessingTask**, now it will be an immediate run task which will continue in the background if user leaves the App. Since the previous solution meant the one off task will only run if the device is idle and as often experienced only when device is charging, in practice it means somewhere at night, or not at all during that day, because **BGProcessingTask** is meant for long running tasks. The new solution makes it more in line with Android except it does not support **initialDelay**
+- If you need the old behavior you can use the new iOS only method `Workmanager.registerProcessingTask`:
+  1. Replace `Workmanager().registerOneOffTask` with `Workmanager().registerProcessingTask` in your App
+  1. Replace `WorkmanagerPlugin.registerTask` with `WorkmanagerPlugin.registerBGProcessingTask` in `AppDelegate.swift`
+- Workmanager.registerOneOffTask does not support **initialDelay**
+- Workmanager.registerOneOffTask now supports **inputData** which was always returning null in the previous solution
+- Workmanager.registerOneOffTask now does NOT require `WorkmanagerPlugin.registerTask` call in `AppDelegate.swift` hence remove the call
+
+### One off tasks
+iOS supports **One off tasks** only on iOS 13+ with a few basic constraints:
+
+`registerOneOffTask` starts immediately. It might run for only 30 seconds due to iOS restrictions.
 
 ```dart
 Workmanager().registerOneOffTask(
   "task-identifier",
   simpleTaskKey, // Ignored on iOS
-  initialDelay: Duration(minutes: 30),
-  constraints: Constraints(
-    // connected or metered mark the task as requiring internet
-    networkType: NetworkType.connected,
-    // require external power
-    requiresCharging: true,
-  ),
+  initialDelay: Duration(minutes: 30), // Ignored on iOS
   inputData: ... // fully supported
 );
 ```
 
+### Periodic tasks
+iOS supports two types of **Periodic tasks**:
+- On iOS 12 and lower you can use deprecated Background Fetch API, see [iOS Setup](./IOS_SETUP.md), even though the API is
+deprecated by iOS it still works on iOS 13+ as of writing this article
+
+- `registerPeriodicTask` is only supported on iOS 13+, it might run for only 30 seconds due to iOS restrictions, but doesn't start immediately, rather iOS will schedule it as per user's App usage pattern.
+
+> ⚠️ On iOS 13+, adding a `BGTaskSchedulerPermittedIdentifiers` key to the Info.plist for new `BGTaskScheduler` API disables the `performFetchWithCompletionHandler` and `setMinimumBackgroundFetchInterval`
+methods, which means you cannot use both old Background Fetch and new `registerPeriodicTask` at the same time, you have to choose one based on your minimum iOS target version. 
+For details see [Apple Docs](https://developer.apple.com/documentation/uikit/app_and_environment/scenes/preparing_your_ui_to_run_in_the_background/using_background_tasks_to_update_your_app)
+
+To use `registerPeriodicTask` first register the task in `Info.plist` and `AppDelegate.swift` [iOS Setup](./IOS_SETUP.md). Unlike Android, for iOS you have to set the frequency in `AppDelegate.swift`. The frequency is not guaranteed rather iOS will schedule it as per user's App usage pattern, iOS might take a few days to learn usage pattern. In reality frequency just means do not repeat the task before x seconds/minutes. If frequency is not provided it will default to 15 minutes.
+
+```objc
+// Register a periodic task with 20 minutes frequency. The frequency is in seconds.
+WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "be.tramckrijte.workmanagerExample.iOSBackgroundAppRefresh", frequency: NSNumber(value: 20 * 60))
+```
+
+Then schedule the task from your App
+```dart
+const iOSBackgroundAppRefresh = "be.tramckrijte.workmanagerExample.iOSBackgroundAppRefresh";
+Workmanager().registerPeriodicTask(
+  iOSBackgroundAppRefresh,
+  iOSBackgroundAppRefresh,
+  initialDelay: Duration(seconds: 10),
+  frequency: Duration(hours: 1), // Ignored on iOS, rather set in AppDelegate.swift
+  inputData: ... // Not supported
+);
+```
+
+For more information see [BGAppRefreshTask](https://developer.apple.com/documentation/backgroundtasks/bgapprefreshtask)
+
+### Processing tasks
+iOS supports **Processing tasks** only on iOS 13+ which can run for more than 30 seconds.
+
+`registerProcessingTask` is a long running one off background task, currently only for iOS. It can be run for more than 30 seconds but doesn't start immediately, rather iOS might schedule it when device is idle and charging.
+Processing tasks are for long processes like data processing and app maintenance. Processing tasks can run for minutes, but the system can interrupt these.
+iOS might terminate any running background processing tasks when the user starts using the device.
+For more information see [BGProcessingTask](https://developer.apple.com/documentation/backgroundtasks/bgprocessingtask)
+
+```dart
+const iOSBackgroundProcessingTask = "be.tramckrijte.workmanagerExample.iOSBackgroundProcessingTask";
+Workmanager().registerProcessingTask(
+  iOSBackgroundProcessingTask,
+  iOSBackgroundProcessingTask,
+  initialDelay: Duration(minutes: 2),
+  constraints: Constraints(
+    // Connected or metered mark the task as requiring internet
+    networkType: NetworkType.connected,
+    // Require external power
+    requiresCharging: true,
+  ),
+);
+```
+
+### Background App Refresh permission
+On iOS user can disable `Background App Refresh` permission anytime, hence background tasks can only run if user has granted the permission.
+With `Workmanager.checkBackgroundRefreshPermission` you can check whether background app refresh is enabled. If it is not enabled you might ask 
+the user to enable it in app settings.
+
+```dart
+if (Platform.isIOS) {
+  final hasPermission = await Workmanager().checkBackgroundRefreshPermission();
+  if (hasPermission != BackgroundRefreshPermissionState.available){
+    // Inform the user that background app refresh is disabled
+  }
+}
+```
+
 For more information see the [BGTaskScheduler documentation](https://developer.apple.com/documentation/backgroundtasks).
+
+### Print scheduled tasks
+On iOS you can print scheduled tasks using `Workmanager.printScheduledTasks`
+
+It prints task details to console. To be used during development/debugging.
+Currently only supported on iOS and only on iOS 13+.
+
+```dart
+if (Platform.isIOS) {
+  Workmanager().printScheduledTasks();
+  // Prints: [BGTaskScheduler] Task Identifier: iOSBackgroundAppRefresh earliestBeginDate: 2023.10.10 PM 11:10:12
+  // Or:     [BGTaskScheduler] There are no scheduled tasks
+}
+```
+
 
 # Customisation (Android)
 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,7 +7,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:workmanager/workmanager.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(home: MyApp()));
 
 const simpleTaskKey = "be.tramckrijte.workmanagerExample.simpleTask";
 const rescheduledTaskKey = "be.tramckrijte.workmanagerExample.rescheduledTask";
@@ -17,21 +17,39 @@ const simplePeriodicTask =
     "be.tramckrijte.workmanagerExample.simplePeriodicTask";
 const simplePeriodic1HourTask =
     "be.tramckrijte.workmanagerExample.simplePeriodic1HourTask";
+const iOSBackgroundAppRefresh =
+    "be.tramckrijte.workmanagerExample.iOSBackgroundAppRefresh";
+const iOSBackgroundProcessingTask =
+    "be.tramckrijte.workmanagerExample.iOSBackgroundProcessingTask";
 
-@pragma(
-    'vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+final List<String> allTasks = [
+  simpleTaskKey,
+  rescheduledTaskKey,
+  failedTaskKey,
+  simpleDelayedTask,
+  simplePeriodicTask,
+  simplePeriodic1HourTask,
+  iOSBackgroundAppRefresh,
+  iOSBackgroundProcessingTask,
+];
+
+// Pragma is mandatory if the App is obfuscated or using Flutter 3.1+
+@pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.reload();
+
+    print("$task started. inputData = $inputData");
+    await prefs.setString(task, 'Last ran at: ${DateTime.now().toString()}');
+
     switch (task) {
       case simpleTaskKey:
-        print("$simpleTaskKey was executed. inputData = $inputData");
-        final prefs = await SharedPreferences.getInstance();
-        prefs.setBool("test", true);
+        await prefs.setBool("test", true);
         print("Bool from prefs: ${prefs.getBool("test")}");
         break;
       case rescheduledTaskKey:
         final key = inputData!['key']!;
-        final prefs = await SharedPreferences.getInstance();
         if (prefs.containsKey('unique-$key')) {
           print('has been running before, task is successful');
           return true;
@@ -52,13 +70,24 @@ void callbackDispatcher() {
       case simplePeriodic1HourTask:
         print("$simplePeriodic1HourTask was executed");
         break;
-      case Workmanager.iOSBackgroundTask:
-        print("The iOS background fetch was triggered");
+      case iOSBackgroundAppRefresh:
+        // To test, follow the instructions on https://developer.apple.com/documentation/backgroundtasks/starting_and_terminating_tasks_during_development
+        // and https://github.com/fluttercommunity/flutter_workmanager/blob/main/IOS_SETUP.md
         Directory? tempDir = await getTemporaryDirectory();
         String? tempPath = tempDir.path;
         print(
             "You can access other plugins in the background, for example Directory.getTemporaryDirectory(): $tempPath");
         break;
+      case iOSBackgroundProcessingTask:
+        // To test, follow the instructions on https://developer.apple.com/documentation/backgroundtasks/starting_and_terminating_tasks_during_development
+        // and https://github.com/fluttercommunity/flutter_workmanager/blob/main/IOS_SETUP.md
+        // Processing tasks are started by iOS only when phone is idle, hence
+        // you need to manually trigger by following the docs and putting the App to background
+        await Future<void>.delayed(Duration(seconds: 40));
+        print("$task finished");
+        break;
+      default:
+        return Future.value(false);
     }
 
     return Future.value(true);
@@ -71,6 +100,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool workmanagerInitialized = false;
+  String _prefsString = "empty";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -90,11 +122,23 @@ class _MyAppState extends State<MyApp> {
                 ),
                 ElevatedButton(
                   child: Text("Start the Flutter background service"),
-                  onPressed: () {
-                    Workmanager().initialize(
-                      callbackDispatcher,
-                      isInDebugMode: true,
-                    );
+                  onPressed: () async {
+                    if (Platform.isIOS) {
+                      final hasPermission = await Workmanager()
+                          .checkBackgroundRefreshPermission();
+                      if (hasPermission !=
+                          BackgroundRefreshPermissionState.available) {
+                        _showNoPermission(context, hasPermission);
+                        return;
+                      }
+                    }
+                    if (!workmanagerInitialized) {
+                      Workmanager().initialize(
+                        callbackDispatcher,
+                        isInDebugMode: true,
+                      );
+                      setState(() => workmanagerInitialized = true);
+                    }
                   },
                 ),
                 SizedBox(height: 16),
@@ -171,12 +215,54 @@ class _MyAppState extends State<MyApp> {
                     onPressed: Platform.isAndroid
                         ? () {
                             Workmanager().registerPeriodicTask(
-                              simplePeriodicTask,
+                              simplePeriodic1HourTask,
                               simplePeriodic1HourTask,
                               frequency: Duration(hours: 1),
                             );
                           }
                         : null),
+
+                // Currently we cannot provide frequency for iOS, hence it will be
+                // minimum 15 minutes after which iOS will reschedule
+                ElevatedButton(
+                  child: Text('Register Periodic Background App Refresh (iOS)'),
+                  onPressed: Platform.isIOS
+                      ? () async {
+                          if (!workmanagerInitialized) {
+                            _showNotInitialized();
+                            return;
+                          }
+                          await Workmanager().registerPeriodicTask(
+                            iOSBackgroundAppRefresh,
+                            iOSBackgroundAppRefresh,
+                            initialDelay: Duration(seconds: 10),
+                            inputData: <String, dynamic>{}, //ignored on iOS
+                          );
+                        }
+                      : null,
+                ),
+
+                // This task runs only once, to perform a time consuming task at
+                // a later time decided by iOS.
+                // Processing tasks run only when the device is idle. iOS might
+                // terminate any running background processing tasks when the
+                // user starts using the device.
+                ElevatedButton(
+                  child: Text('Register BackgroundProcessingTask (iOS)'),
+                  onPressed: Platform.isIOS
+                      ? () async {
+                          if (!workmanagerInitialized) {
+                            _showNotInitialized();
+                            return;
+                          }
+                          await Workmanager().registerProcessingTask(
+                            iOSBackgroundProcessingTask,
+                            iOSBackgroundProcessingTask,
+                            initialDelay: Duration(seconds: 20),
+                          );
+                        }
+                      : null,
+                ),
                 SizedBox(height: 16),
                 Text(
                   "Task cancellation",
@@ -189,11 +275,79 @@ class _MyAppState extends State<MyApp> {
                     print('Cancel all tasks completed');
                   },
                 ),
+                SizedBox(height: 15),
+                ElevatedButton(
+                  child: Text('Refresh stats'),
+                  onPressed: _refreshStats,
+                ),
+                SizedBox(height: 10),
+                SingleChildScrollView(
+                  child: Text(
+                    'Task run stats:\n'
+                    '${workmanagerInitialized ? '' : 'Workmanager not initialized'}'
+                    '\n$_prefsString',
+                  ),
+                ),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  // Refresh/get saved prefs
+  void _refreshStats() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.reload();
+
+    _prefsString = '';
+    for (final task in allTasks) {
+      _prefsString = '$_prefsString \n$task:\n${prefs.getString(task)}\n';
+    }
+
+    if (Platform.isIOS) {
+      Workmanager().printScheduledTasks();
+    }
+
+    setState(() {});
+  }
+
+  void _showNotInitialized() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Workmanager not initialized'),
+          content: Text('Workmanager is not initialized, please initialize'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showNoPermission(
+      BuildContext context, BackgroundRefreshPermissionState hasPermission) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('No permission'),
+          content: Text('Background app refresh is disabled, please enable in '
+              'App settings. Status ${hasPermission.name}'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
     );
   }
 }
