0810dedfb3aaa564d6cf7a2e86828b569089b74d
charafau <gorudonu@gmail.com>
2024-08-21 03:06:54 +0900

    refactor test workflow to use script

---
+++ .github/workflows/scripts/install-tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+flutter config --no-analytics
+flutter pub global activate melos 3.0.0
+echo "$HOME/.pub-cache/bin" >>$GITHUB_PATH
+echo "$HOME/AppData/Local/Pub/Cache/bin" >>$GITHUB_PATH
+echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >>$GITHUB_PATH
+echo "$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin" >>$GITHUB_PATH
