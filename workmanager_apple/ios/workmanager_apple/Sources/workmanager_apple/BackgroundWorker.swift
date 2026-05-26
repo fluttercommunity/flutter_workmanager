cfbf11c2f88845ada24c8a3f4b995da0d9766016
Seungho Hong <seunghonice@gmail.com>
2026-05-26 14:56:40 +0900

    fix: SPM 빌드 시 UIKit import 누락 수정
    CocoaPods에서는 모듈 전체가 하나로 컴파일되어 다른 파일의 import가 공유되지만,
    SPM에서는 파일 단위로 import가 필요함

--- workmanager_apple/ios/workmanager_apple/Sources/workmanager_apple/BackgroundWorker.swift
+++ workmanager_apple/ios/workmanager_apple/Sources/workmanager_apple/BackgroundWorker.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 #if os(iOS)
 import Flutter
--- workmanager_apple/ios/workmanager_apple/Sources/workmanager_apple/Extensions.swift
+++ workmanager_apple/ios/workmanager_apple/Sources/workmanager_apple/Extensions.swift
@@ -5,7 +5,7 @@
 //  Created by Kymer Gryson on 13/08/2019.
 //
 
-import Foundation
+import UIKit
 
 extension UIBackgroundFetchResult: CustomDebugStringConvertible {
     public var debugDescription: String {
